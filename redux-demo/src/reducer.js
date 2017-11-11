003131a282403c4863b9278a17292419ebffce27
Dawid Matuszewski <dawmatuszewski@gmail.com>
2017-11-11 22:06:44 +0100

    Zadanie 19.7

---
+++ redux-demo/src/reducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import comments from './comments.js';
+import users from './users.js';
+
+const reducer = combineReducers({
+  comments,
+  users
+});
+
+export {
+  reducer
+}; 
